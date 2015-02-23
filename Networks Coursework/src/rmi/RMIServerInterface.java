951ce02d03ac0d25218719429ca43216ed12b9e3
ahairyrat <patengelbert@hotmail.co.uk>
2015-02-23 11:27:54 +0000

    added comments to interface

--- Networks Coursework/src/rmi/RMIServerInterface.java
+++ Networks Coursework/src/rmi/RMIServerInterface.java
@@ -5,6 +5,14 @@ import java.rmi.RemoteException;
 
 import common.MessageInfo;
 
+/**
+ * The interface for an RMIServer class. It allows for teh passing of messages
+ * to the implementation. It can also be bound to the registry to allow for
+ * network communication
+ * 
+ * @author pe313
+ *
+ */
 public interface RMIServerInterface extends Remote {
 	public <T> void receiveMessage(MessageInfo info, T message)
 			throws RemoteException;
